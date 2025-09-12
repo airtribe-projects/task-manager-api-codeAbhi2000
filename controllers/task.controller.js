1f3803cd5612dab0821b1cada6aabb0eec92d302
ABHISHEK BADIGER <86523668+codeAbhi2000@users.noreply.github.com>
2025-09-12 22:34:16 +0530

    completed the all end points

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,7 +1,74 @@
 const tasks = require("../task.json");
 
-console.log(tasks);
-
 exports.createTask = (req, res) => {
-    
+   const  {title , description, completed} = req.body
+   if (!title || !description || completed === undefined){
+    return res.status(400).send()
+   }
+
+   const newTask = {
+    id : parseInt(tasks.tasks.lenghth + 1),
+    title,
+    description,
+    completed
+   }
+
+    tasks.tasks.push(newTask)
+
+    return res.status(201).send()
+}
+
+exports.getAllTask = (req,res) =>{
+    return res.status(200).send(
+        [...tasks.tasks]
+    )
+}
+
+exports.getTaskById = (req,res) =>{
+    const id = parseInt(req.params.id)
+    const task = tasks.tasks?.find((t) => t.id === id)
+    if (!task){
+        return res.status(404).send()
+    }
+    return res.status(200).send({
+        ...task
+    })
+}
+
+exports.updateTask = (req,res) =>{
+    const id = parseInt(req.params.id)
+    const taskIndex = tasks.tasks?.findIndex((t) => t.id === id)
+    if (taskIndex === -1){
+        return res.status(404).send()
+    }
+
+    const {title, description, completed} = req.body
+    if ((!title || typeof title !== "string") || (!description || typeof description !== "string") || (typeof completed !== "boolean")){
+        return res.status(400).send()
+    }
+
+    tasks.tasks[taskIndex] = {
+        id,
+        title,
+        description,
+        completed
+    }
+
+    return res.status(200).send()
+}
+
+exports.deleteTask = (req,res) =>{
+    const id = parseInt(req.params.id)
+    const taskIndex = tasks.tasks.findIndex((t) => t.id === id)
+    if (taskIndex === -1){
+        return res.status(404).send({
+            message : "task not found"
+        })
+    }
+
+    tasks.tasks.splice(taskIndex, 1)
+
+    return res.status(200).send({
+        message : "task deleted successfully"
+    })
 }
